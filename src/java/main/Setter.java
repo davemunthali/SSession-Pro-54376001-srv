c94718bf2e4e85be4c8964708b99adc3c1401488
daviemunthali <davemunthali@gmail.com>
2019-04-06 16:28:32 +0200

    set a session and redirect in Setter

--- src/java/main/Setter.java
+++ src/java/main/Setter.java
@@ -7,10 +7,13 @@ package main;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.websocket.Session;
 
 /**
  *
@@ -29,18 +32,12 @@ public class Setter extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Setter</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Setter at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        
+        HttpSession s = request.getSession();
+        s.setAttribute("name","Davie Munthali");
+        RequestDispatcher ds = request.getRequestDispatcher("Getter");
+        ds.forward(request, response);
+        
         }
     }
 
